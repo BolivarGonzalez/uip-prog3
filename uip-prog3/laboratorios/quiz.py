80ebd21c21c2302023faf2240c6b93f18b498914
BolivarGonzalez <its_bege@outlook.com>
2015-09-21 22:06:22 -0600

    entrega del quiz numero 1

---
+++ uip-prog3/laboratorios/quiz.py
@@ -0,0 +1,13 @@
+# CLASE 3
+# Autor: Aristides Ortega
+#
+# Instrucciones: Calcular el area y perimetro de un cuadrado
+base = 5
+altura = 7
+metro = 100
+pulgada = 2.54
+area = base*altura
+perimetro = 2*(base+altura)
+print('El area es de '+str(area)+ ' y el perimetro es de '+str(perimetro)+' centimetros')
+print('El area es de '+str((area)/metro)+ ' y el perimetro es de '+str((perimetro)/metro)+' metros')
+print('El area es de '+str((area)/pulgada)+ ' y el perimetro es de '+str((perimetro)/pulgada)+' pulgadas')
