f492f9ffbd08dc0a3ceca2ced3d7311666e83483
BolivarGonzalez <its_bege@outlook.com>
2015-09-21 22:00:24 -0600

    entrega Quiz 1

---
+++ uip-prog3/laboratorios/quiz1.py
@@ -0,0 +1,17 @@
+#1m = 100 cm       1plg = 2.54 cm
+base = 5
+altura = 7
+area = base * altura
+perimetro = 2 * base + 2 * altura
+apl = area / 100
+amt = area / 2.54
+ppl = perimetro / 100
+pmt = perimetro / 2.54
+
+
+print('Area Del Rectangulo En Centimetro Es ' + str(area))
+print('Area Del Rectangulo En Pulgada Es ' + str(apl))
+print('Area Del Rectangulo En Metro Es ' + str(amt))
+print('Perimetro Del Rectangulo En Centimetro Es ' + str(perimetro)) 
+print('Perimetro Del Rectangulo En Pulgadas Es ' + str(ppl))
+print('Perimetro Del Rectangulo En Metros Es ' + str(pmt))
